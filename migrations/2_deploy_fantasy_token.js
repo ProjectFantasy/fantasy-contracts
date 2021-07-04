ded39d43c3d1deefb00804841520fc4a6c10e683
SFzxc <hoangphuocvanlong@gmail.com>
2021-07-04 09:30:23 +0700

    chore: remove consolelog

--- migrations/2_deploy_fantasy_token.js
+++ migrations/2_deploy_fantasy_token.js
@@ -2,6 +2,5 @@ const FantasyToken = artifacts.require("FantasyToken");
 
 module.exports = function (deployer) {
   const tokenOwner = process.env.TOKEN_OWNER;
-  console.log(deployer);
   deployer.deploy(FantasyToken, tokenOwner);
 };
